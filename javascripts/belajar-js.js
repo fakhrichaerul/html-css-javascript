7b3409103d1f9d94c2e99405c4f4a61403c62eec
fakhrichaerul <fakhrichaerul@gmail.com>
2021-04-04 09:38:57 +0700

    commit pertama

---
+++ javascripts/belajar-js.js
@@ -0,0 +1,7 @@
+document.getElementById("demo").innerHTML = "Hello JavaScript!";
+
+document.getElementById("demo").innerHTML = Date();
+
+document.getElementById("demo").style.fontSize = "25px";
+document.getElementById("demo").style.color = "red";
+document.getElementById("demo").style.backgroundColor = "yellow";
